5c506799826840a781a67c9ac246548b5cc6b4f0
Fran <franjoseperezlozano@gmail.com>
2025-03-20 09:55:56 +0100

    Seeders

---
+++ app/Database/Seeds/CategoriaSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use App\Controllers\Dashboard\Categoria;
+use App\Models\CategoriaModel;
+use CodeIgniter\Database\Seeder;
+
+class CategoriaSeeder extends Seeder
+{
+    public function run()
+    {
+        $categoriaModel = new CategoriaModel();
+
+        $categoriaModel->where('id >=', 1)->delete();
+
+        for ($i = 0; $i <= 20; $i++) {
+            $categoriaModel->insert([
+            'titulo' => "Categoria $i"
+            ]);
+        }
+    }
+}
