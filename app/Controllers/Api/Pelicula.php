238f946c69d83e85c3c4a06cf88f2e3bab2d0f03
Fran <franjoseperezlozano@gmail.com>
2025-03-20 09:29:05 +0100

    pelicula api

--- app/Controllers/Api/Pelicula.php
+++ app/Controllers/Api/Pelicula.php
@@ -1,5 +1,7 @@
 <?php
 namespace App\Controllers\Api;
+
+use App\Models\PeliculaModel;
 use CodeIgniter\RESTful\ResourceController;
 
 class Pelicula extends ResourceController{
@@ -9,6 +11,47 @@ class Pelicula extends ResourceController{
 
     public function index(){
         return $this->respond($this->model->findAll());
+    }
+
+    public function show($id = null){
+        return $this->respond($this->model->find($id));
+    }
+
+    public function create(){
+
+        if($this->validate('peliculas')){
+            $id = $this->model->insert([
+                'titulo' => $this->request->getPost('titulo'),
+                'descripcion' => $this->request->getPost('descripcion'),
+            ]);
+
+        } else {
+           return $this->respond($this->validator->getErrors(), 400);
+        }
+
+        return $this->respond($id);
+    }
+
+    public function update($id = null){
+
+        if($this->validate('peliculas')){
+            $this->model->update($id, [
+                'titulo' => $this->request->getPost('titulo'),
+                'descripcion' => $this->request->getPost('descripcion'),
+            ]);
+
+        } else {
+            return $this->respond($this->validator->getErrors(), 400);
+        }
+
+        return $this->respond($id);
+    }
+
+    public function delete($id = null){
+
+        $this->model->delete($id);
+
+        return $this->respond('ok');
     }
 
 }
